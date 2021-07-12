538f1b091bf374ed0fa6ca1077c788c78af7f7ff
yolothedino <1902664@sit.singaporetech.edu.sg>
2021-07-13 02:29:53 +0800

    updated model training file to account for dataset more organically (no need to manually set train/test folders in dataset)

--- Food101Model.py
+++ Food101Model.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 import tensorflow.keras.backend as K
 from tensorflow.keras import regularizers
 from tensorflow.keras.applications.inception_v3 import InceptionV3
-from tensorflow.keras.applications.efficientnet import EfficientNetB2   # seems to work?
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D, AveragePooling2D
@@ -19,6 +19,7 @@ from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.regularizers import l2
 from tensorflow import keras
 import numpy as np
+import matplotlib.pyplot as plt
 
 import tensorflow as tf
 print(tf.__version__)
@@ -26,59 +27,152 @@ print(tf.test.gpu_device_name())
 
 K.clear_session()
 
-n_classes = 35   # Testing 35 classes of SG foods, so n_classes = 35
-img_width, img_height = 299, 299    # may need to adjust based on efficientnet needs. keeping at default of 299/299
-train_data_dir = 'foodSG/train'
-validation_data_dir = 'foodSG/test'
-nb_train_samples = 6595 # i.e. amount of images in foodsg/train folder
-nb_validation_samples = 350 # i.e. amount of images in foodsg/test folder
+n_classes = 59   # based on amount of classes in dataset. change when dataset changes.
 
-# batch size - higher = faster learning, but more memory needed.
-# was 20 but hit OOM. try 10. Setting too high (e.g. 10) WILL MAKE YOUR COMPUTER CRASH
+img_width, img_height, channels = 299, 299, 3
+dataset_dir = 'foodSG'   # dataset images should be unzipped here. they should be in folders (e.g. chicken rice, laksa)
 
-batch_size = 10
+# how it should look like:
+# PROJECT DIRECTORY
+# > foodsg
+# >> chicken rice (example)
+# >>> 1.jpg
+# >>> 2.jpg
+# >>> ... (and so on)
+# >> duck rice (example)
+# >>> 1.jpg
+# >>> 2.jpg
+# >>> ... (and so on)
+
+train_data_dir = 'foodSG/train' # not needed, but useful in future
+validation_data_dir = 'foodSG/test' # not needed, but useful in future
+nb_train_samples = 10000 # i.e. amount of images in foodsg/train folder
+nb_validation_samples = 1180 # i.e. amount of images in foodsg/test folder. should be n_classes*10
+
+
+batch_size = 2  # higher values = faster training, but more memory needed
+# recommended batch size values are 16, 32, 64, 128, 256
+
+EPOCHS = 20     # more epochs = better accuracy, but will plateau eventually
+
+# below based on: https://stackoverflow.com/questions/42443936/keras-split-train-test-set-when-using-imagedatagenerator
+
+# creating image data generator for train images, to make up for small dataset
 
 train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
+                 rescale=1./255,
+                 rotation_range=40,
+                 width_shift_range=0.2,
+                 height_shift_range=0.2,
+                 shear_range=0.2,
+                 zoom_range=0.2,
+                 horizontal_flip=True,
+                 fill_mode='nearest',
+                 validation_split=0.2)  # 20% of the dataset is used as the test/validate set
 
-test_datagen = ImageDataGenerator(rescale=1. / 255)
+# creating image data generator for validation images. they should not be augmented other than rescaling.
+# still need ImageDatGenerator for the validation_split
+
+test_datagen = ImageDataGenerator(
+                rescale=1. / 255,
+                validation_split=0.2)
+
+#train_generator = train_datagen.flow_from_directory(train_data_dir,target_size=(img_height, img_width),batch_size=batch_size,class_mode='categorical')
+
+#validation_generator = test_datagen.flow_from_directory(validation_data_dir,target_size=(img_height, img_width),batch_size=batch_size,class_mode='categorical')
 
 train_generator = train_datagen.flow_from_directory(
-    train_data_dir,
+    dataset_dir,
     target_size=(img_height, img_width),
     batch_size=batch_size,
-    class_mode='categorical')
+    class_mode='categorical',
+    subset='training',
+    seed=123,
+    shuffle=True)
 
 validation_generator = test_datagen.flow_from_directory(
-    validation_data_dir,
+    dataset_dir,     # same directory as before - but validation split will mean different images are used
     target_size=(img_height, img_width),
     batch_size=batch_size,
-    class_mode='categorical')
+    class_mode='categorical',
+    subset='validation',
+    seed=123,
+    shuffle=True)
 
+# not sure if train_generator and validation_generator has any mix of images.
+# mixing = not good, validation should not mix with train, leads to misleading model
 
-efficient_net_model = EfficientNetB2(weights='imagenet', include_top=False, input_shape = (299,299,3))
-x = efficient_net_model.output
+# instantiate the model base
+# weights - use the weights that was acquired when the model is initially trained on the ImageNet dataset
+# include_top=False will essentially allow the model to be fine-tuned. i.e. the 'top layer' is excluded, allowing for our own 'top layer'
+# input_shape - standardises all inputs to be of a certain dimension
+
+nutricare_model= MobileNetV2(weights='imagenet',
+                             include_top=False,
+                             input_shape = (img_height,img_width,channels)) # MV uses 299, 299, 3
+x = nutricare_model.output
 x = GlobalAveragePooling2D()(x)
 x = Dense(128,activation='relu')(x)
 x = Dropout(0.2)(x)
 
-# REMEMBER TO CHANGE BELOW NUMBER VALUE TO WHICHEVER AMOUNT OF CLASSES IN YOUR DATASET
-predictions = Dense(35,kernel_regularizer=regularizers.l2(0.005), activation='softmax')(x)
+# REMEMBER TO CHANGE BELOW NUMBER VALUE TO WHICHEVER AMOUNT OF CLASSES IN YOUR DATASET. or just use n_classes
+predictions = Dense(n_classes,kernel_regularizer=regularizers.l2(0.005), activation='softmax')(x)
 
-model = Model(inputs=efficient_net_model.input, outputs=predictions)
-model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
-checkpointer = ModelCheckpoint(filepath='best_model_3class_sept.hdf5', verbose=1, save_best_only=True)
-csv_logger = CSVLogger('history.log')
+model = Model(inputs=nutricare_model.input, outputs=predictions)
+
+base_learning_rate = 0.0001
+
+model.compile(optimizer=SGD(lr=base_learning_rate, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+checkpointer = ModelCheckpoint(filepath='MN_best_model_3class_sept.hdf5', verbose=1, save_best_only=True)
+csv_logger = CSVLogger('MN_history.log')
 
 history = model.fit_generator(train_generator,
                     steps_per_epoch = nb_train_samples // batch_size,
                     validation_data=validation_generator,
                     validation_steps=nb_validation_samples // batch_size,
-                    epochs=20,
+                    epochs=EPOCHS,
                     verbose=1,
                     callbacks=[csv_logger, checkpointer])
 
-model.save('model_trained.h5')
+# plotting after training
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_x = range(len(acc))
+
+plt.plot(epochs_x, acc, 'bo', label='Training acc')
+plt.plot(epochs_x, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs_x, loss, 'bo', label='Training loss')
+plt.plot(epochs_x, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+model.save('MN_model_trained.h5')
+
+# ====
+
+train_generator = tf.keras.preprocessing.image_dataset_from_directory(
+  train_data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+validation_generator = tf.keras.preprocessing.image_dataset_from_directory(
+  train_data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
