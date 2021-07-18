0c6a553ddf05f1ecaf1cfcb08fbbde4799229a4d
yolothedino <1902664@sit.singaporetech.edu.sg>
2021-07-19 01:47:53 +0800

    improved prediction script to give top 5 results, added more comments to clarify code

--- prediction_alternative.py
+++ prediction_alternative.py
@@ -1,28 +1,18 @@
-import tensorflow as tf
 from PIL.ImageOps import crop
-from tensorflow import keras
-from tensorflow.keras import backend as K
-from tensorflow.keras.layers import Dense, Activation,Dropout,Conv2D, MaxPooling2D,BatchNormalization, Flatten
-from tensorflow.keras.optimizers import Adam, Adamax
-from tensorflow.keras.metrics import categorical_crossentropy
-from tensorflow.keras import regularizers
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Model, load_model, Sequential
 import numpy as np
 import pandas as pd
 import shutil
-import time
 import cv2 as cv2
-from tqdm import tqdm
-from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import imshow
 import os
+import tensorflow as tf
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"   # prevent GPU usage
+
 import seaborn as sns
 sns.set_style('darkgrid')
-from PIL import Image
-from sklearn.metrics import confusion_matrix, classification_report
-from IPython.core.display import display, HTML
+
 # stop annoying tensorflow warning messages
 import logging
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
@@ -60,9 +50,19 @@ def classify(sdir, csv_path,  model_path, crop_image = False):
         path_list.append(os.path.join(sdir,f))
     print (' Model is being loaded- this will take about 10 seconds')
     model=load_model(model_path)
+
     image_count=len(path_list)
+
+    #print("Image count: " + str(image_count))   # only give 1 - guess this means the given image
+
     index_list=[]
     prob_list=[]
+
+    # stuff for top-5
+    food_list_5=[]
+    prob_list_5=[]
+    # end of stuff for top-5
+
     cropped_image_list=[]
     good_image_count=0
     for i in range (image_count):
@@ -77,21 +77,44 @@ def classify(sdir, csv_path,  model_path, crop_image = False):
             cropped_image_list.append(img)
             img=img*s2 - s1
             img=np.expand_dims(img, axis=0)
+
+            # prediction rates are given here
             p= np.squeeze (model.predict(img))
+
+            #print("Printing max...")
+            #print(p)
             index=np.argmax(p)
+
+            probability_array, food_index_array = tf.nn.top_k(p,5)
+            # values = probabilities. indices = index of food items
+
+            #print("Printing values")    # prints the array of top 5 probabilities
+            #print(probability_array.numpy())
+            for i in probability_array.numpy():
+                #print(i)
+                prob_list_5.append(i)
+
+            #print("Printing indices")   # prints the array of top 5 predicted food (by their index number)
+            #print(food_index_array.numpy())
+            #for i in food_index_array.numpy():
+            #    print(i)
             prob=p[index]
             index_list.append(index)
             prob_list.append(prob)
-    if good_image_count==1:
+    if good_image_count==1: # provided image is good and probability matrix has been acquired
         class_name= class_df['class'].iloc[index_list[0]]
+
+        # list of food index numbers are nice
+        # but list of food names are better
+        for i in food_index_array.numpy():
+            food_name = class_df['class'].iloc[i]   # tally to the .csv file for the food name
+            #print(food_name)
+            food_list_5.append(food_name)
+
         probability= prob_list[0]
-        img=cropped_image_list [0]
-        plt.title(class_name, color='blue', fontsize=16)
-        plt.axis('off')
-        plt.imshow(img)
-        return class_name, probability
+        return class_name, probability, food_list_5, prob_list_5
     elif good_image_count == 0:
-        return None, None
+        return None, None, None, None
     most=0
     for i in range (len(index_list)-1):
         key= index_list[i]
@@ -122,7 +145,7 @@ store_path=os.path.join(working_dir, 'storage')
 if os.path.isdir(store_path):
     shutil.rmtree(store_path)
 os.mkdir(store_path)
-img_path='whiskey.jpg'
+img_path='test images/char_siew_rice.jpg'
 img=cv2.imread(img_path)
 split=os.path.split(img_path)
 file_name=split[1]
@@ -133,8 +156,14 @@ cv2.imwrite(dst_path, img)
 # check if the directory was created and image stored
 print (os.listdir(working_dir))
 
-csv_path='class_dict.csv' # path to class_dict.csv
-model_path='EN_Model.h5' # path to the trained model
-class_name, probability=classify(store_path, csv_path,  model_path, crop_image = False) # run the classifier
-msg=f'image {full_name} is of {class_name} with a probability of {probability * 100: 6.2f} %'
-print_in_color(msg, (0,255,255), (65,85,55))
+csv_path='class_dict_foodsg.csv' # path to class_dict.csv
+model_path='EN_model_colab_prefinetune.hdf5' # path to the trained model
+class_name, probability, name_list, probability_list =classify(store_path, csv_path,  model_path, crop_image = False) # run the classifier
+#msg=f'image {full_name} is of {class_name} with a probability of {probability * 100: 6.2f} %'
+
+for index, item in enumerate(probability_list):
+    count = index + 1
+    item_formatted = "{:.2%}".format(item)
+    print(str(count) + ". " + name_list[index] + " - " + item_formatted)
+
+#print_in_color(msg, (0,255,255), (65,85,55))
